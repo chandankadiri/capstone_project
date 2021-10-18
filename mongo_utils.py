773b69151ee4ef61392fe7f1cb6b06806dd07c15
Chandan K <85396243+chandankadiri@users.noreply.github.com>
2021-10-18 22:01:00 +0530

    Create mongo_utils.py
    code with spark processing

---
+++ mongo_utils.py
@@ -0,0 +1,47 @@
+import pymongo
+import pandas as pd
+from pymongo import MongoClient
+
+def get_mongo_connection():
+    try:
+        client = MongoClient()
+        db = client.capstone
+        collection = db.news_articles
+        return collection
+    except Exception as err:
+        raise Exception(err)
+
+def read_from_mongo():
+    try:
+        collection = get_mongo_connection()
+        df = pd.DataFrame(list(collection.find()))
+        print(f'returned {df.shape[0]} rows from mongodb')
+        return df
+    except Exception as err:
+        raise Exception(err)
+
+def write_to_mongo(df):
+    try:
+        collection = get_mongo_connection()
+        collection.insert_many(df.to_dict('records'))
+        print(f'inserted {df.shape[0]} rows in to mongodb')
+        return True
+    except Exception as err:
+        raise Exception(err)
+
+def delete_from_mongo(del_type,del_cond):
+    try:
+        collection = get_mongo_connection()
+        if del_type=='many':
+            collection.delete_many(del_cond)
+        elif del_type=='all':
+            collection.remove( {} )
+        print('deleted  rows from  mongodb')
+        return True
+    except Exception as err:
+        raise Exception(err)
+
+if __name__ == '__main__':
+    df = read_from_mongo()
+    import pdb;pdb.set_trace()
+    #delete_from_mongo('many', {'extra_col1':''})
